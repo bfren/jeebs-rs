425bf8a01cce7333a05fc6041b72c1c052b6f8a5
bfren <bf@bfren.dev>
2023-12-08 15:23:08 +0000

    Adding console-test app

---
+++ apps/console-test/src/main.rs
@@ -0,0 +1,51 @@
+use jeebs::result::*;
+use std::env;
+use thiserror::Error;
+
+fn main() {
+    let share = get_share().handle();
+    println!("The pirate share is {}", share);
+}
+
+fn get_share() -> Result<u64> {
+    let args = env::args().skip(1).collect::<Vec<String>>();
+    let total = args
+        .get(0)
+        .ok_or(Errors::UnableToGetFirstArgument)?
+        .parse::<u64>()
+        .map_err(|_| Errors::UnableToParseFirstArgument)?;
+    let crew_size = args
+        .get(1)
+        .ok_or(Errors::UnableToGetSecondArgument)?
+        .parse::<usize>()
+        .map_err(|_| Errors::UnableToParseSecondArgument)?;
+    Ok(pirate_share(total, crew_size))
+}
+
+fn pirate_share(total: u64, crew_size: usize) -> u64 {
+    let half = total / 2;
+    half / crew_size as u64
+}
+
+#[derive(Error, Debug)]
+enum Errors {
+    #[error("Unable to get first argument")]
+    UnableToGetFirstArgument,
+    #[error("The first argument is not a valid number")]
+    UnableToParseFirstArgument,
+    #[error("Unable to get second argument")]
+    UnableToGetSecondArgument,
+    #[error("The second argument is not a valid number")]
+    UnableToParseSecondArgument
+}
+
+#[test]
+fn returns_correct_share() {
+    let share = pirate_share(10, 2);
+    assert_eq!(2, share);
+}
+
+#[test]
+fn panics_when_no_crew() {
+    pirate_share(10, 0);
+}
